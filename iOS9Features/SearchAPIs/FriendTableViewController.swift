f8bd2e3049781c744d273547b5a6a37734466510
Shane <Shane-Mini>
2015-11-30 13:28:29 +0800

    base init

--- iOS9Features/SearchAPIs/FriendTableViewController.swift
+++ iOS9Features/SearchAPIs/FriendTableViewController.swift
@@ -9,15 +9,15 @@
 import UIKit
 
 class FriendTableViewController: UITableViewController {
+    
+    let datasource = Datasource();
+    var lastSelectedFriend: Person?;
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell");
+        
+        datasource.savePeopleToIndex();
     }
 
     override func didReceiveMemoryWarning() {
@@ -82,14 +82,14 @@ class FriendTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        let destination = segue.destinationViewController as! FriendViewController;
+        
     }
-    */
+
 
 }
